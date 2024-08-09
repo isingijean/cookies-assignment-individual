05ed24205f2dbff2bbb5d88e35450a5fb029392f
isingijean <isingijean@gmail.com>
2024-08-09 18:41:27 +0200

    Add files via upload

---
+++ TEST/src/main/java/Assignment/Q5.java
@@ -0,0 +1,57 @@
+package Assignment;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Q5
+ */
+@WebServlet("/Q5")
+public class Q5 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Q5() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		String uname=request.getParameter("username");
+		String pass=request.getParameter("password");
+		if(uname.equals("rp")&& pass.equals("huye")) {
+		RequestDispatcher rd=request.getRequestDispatcher("WelcomePage");
+		rd.forward(request, response);
+		}else{
+		out.print("Sorry, incorrect username or password, try again");
+		out.print("<a href='login.html'>Go</a>");
+		}}
+
+
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
